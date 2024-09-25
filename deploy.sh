91f6f5d74f4d1f9e241f0fbac5ccd1c3140d87fd
Sujit Ram Dhakal <sujit.dhakal.wrk@gmail.com>
2024-09-25 17:16:53 +0545

    refactor: github-ci

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,11 @@ ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $1@$2 'mkdir -p ~/ecom/deployme
 
 scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ecom/docker-compose.yml $1@$2:~/ecom/deployment/docker-compose.yml
 
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $1@$2 "echo '$3' > ~/ecom/deployment/.env"
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $1@$2 "cat > ~/ecom/deployment/.env <<EOF
+$3
+EOF"
+
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $1@$2 "cat ~/ecom/deployment/.env"
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $1@$2 'chmod 644 ~/ecom/deployment/docker-compose.yml'
-
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $1@$2 'cd ~/ecom/deployment && docker-compose up -d'
-
-sleep 300
-
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $1@$2 'cd ~/ecom/deployment && docker-compose down'
