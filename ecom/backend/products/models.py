7f3e968c31833b4160c73fb4f145f428387bccf4
Sujit Ram Dhakal <sujit.dhakal.wrk@gmail.com>
2024-10-30 20:41:56 +0545

    feat: product image

--- ecom/backend/products/models.py
+++ ecom/backend/products/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
+    image = models.ImageField(upload_to='images')
     price = models.DecimalField(decimal_places=2,max_digits=5)
     stock = models.IntegerField()
 
