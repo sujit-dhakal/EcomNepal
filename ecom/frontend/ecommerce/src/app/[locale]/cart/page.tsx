fb4dce68a509be5facfaac20ff0aa10cf1012322
manish <maneeshbalami@gmail.com>
2024-11-09 18:59:02 +0545

    refactor(cart): updated table format

--- ecom/frontend/ecommerce/src/app/[locale]/cart/page.tsx
+++ ecom/frontend/ecommerce/src/app/[locale]/cart/page.tsx
@@ -68,83 +68,81 @@ const page = () => {
             </div>
           ) : (
             <div>
-              {cartItems.map((item: CartItems) => (
-                <div key={item.product.id}>
-                  <table>
-                    <thead className="bg-gray-100 border-b-2 border-gray-200">
-                      <tr>
-                        <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          Product Id
-                        </th>
-                        <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          Product Name
-                        </th>
-                        <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          Quantity
-                        </th>
-                        <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          Price
-                        </th>
-                        <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg"></th>
-                      </tr>
-                    </thead>
-                    <tbody className="bg-white">
-                      <tr className="border-b-2 border-gray-200">
-                        <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          {item.product.id}
-                        </td>
-                        <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          {item.product.name}
-                        </td>
-                        <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          <input
-                            type="number"
-                            value={item.quantity}
-                            onChange={(e) =>
-                              handleQuantityChange(
-                                item.product.id,
-                                parseInt(e.target.value)
-                              )
-                            }
-                            min={1}
-                            max={5}
-                            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm [&::-webkit-outer-spin-button]:opacity-100 [&::-webkit-inner-spin-button]:opacity-100"
-                          />
-                        </td>
-                        <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          ${item.total_price}
-                        </td>
-                        <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
-                          <button
-                            type="button"
-                            onClick={() => removeItem(item.product.id)}
-                            className="bg-red-900 px-2 py-1 md:px-4 md:py-2 rounded-lg text-white"
-                          >
-                            Remove
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <div className="flex justify-center">
-                    <div className="w-64 py-4 px-8 border-2 text-center border-gray-200 mt-20">
-                      <h1>
-                        <span className="font-semibold">Total Sum: </span>${sum}
-                      </h1>
-                      <button
-                        className="mt-4 bg-gray-500 text-white py-2 px-4 rounded-lg"
-                        onClick={handleProceed}
-                      >
-                        Proceed to Checkout
-                      </button>
-                    </div>
-                  </div>
+              <table>
+                <thead className="bg-gray-100 border-b-2 border-gray-200">
+                  <tr>
+                    <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                      Product Id
+                    </th>
+                    <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                      Product Name
+                    </th>
+                    <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                      Quantity
+                    </th>
+                    <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                      Price
+                    </th>
+                    <th className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg"></th>
+                  </tr>
+                </thead>
+                {cartItems.map((item: CartItems) => (
+                  <tbody key={item.product.id} className="bg-white">
+                    <tr className="border-b-2 border-gray-200">
+                      <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                        {item.product.id}
+                      </td>
+                      <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                        {item.product.name}
+                      </td>
+                      <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                        <input
+                          type="number"
+                          value={item.quantity}
+                          onChange={(e) =>
+                            handleQuantityChange(
+                              item.product.id,
+                              parseInt(e.target.value)
+                            )
+                          }
+                          min={1}
+                          max={5}
+                          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm [&::-webkit-outer-spin-button]:opacity-100 [&::-webkit-inner-spin-button]:opacity-100"
+                        />
+                      </td>
+                      <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                        ${item.total_price}
+                      </td>
+                      <td className="px-3 md:px-6 lg:px-20 py-3 md:py-6 lg:py-5 text-sm lg:text-lg">
+                        <button
+                          type="button"
+                          onClick={() => removeItem(item.product.id)}
+                          className="bg-red-900 px-2 py-1 md:px-4 md:py-2 rounded-lg text-white"
+                        >
+                          Remove
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                ))}
+              </table>
+              <div className="flex justify-center">
+                <div className="w-64 py-4 px-8 border-2 text-center border-gray-200 mt-20">
+                  <h1>
+                    <span className="font-semibold">Total Sum: </span>${sum}
+                  </h1>
+                  <button
+                    className="mt-4 bg-gray-500 text-white py-2 px-4 rounded-lg"
+                    onClick={handleProceed}
+                  >
+                    Proceed to Checkout
+                  </button>
                 </div>
-              ))}
+              </div>
             </div>
           )}
         </div>
-      </div>
+      </div >
     </>
   );
 };
