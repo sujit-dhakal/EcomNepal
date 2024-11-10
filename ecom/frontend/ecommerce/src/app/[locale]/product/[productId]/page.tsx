7a7d272da1e4026feac28e5b3180dcafe212d7a6
Sujit Ram Dhakal <sujit.dhakal.wrk@gmail.com>
2024-11-10 16:59:07 +0545

    feat: css

--- ecom/frontend/ecommerce/src/app/[locale]/product/[productId]/page.tsx
+++ ecom/frontend/ecommerce/src/app/[locale]/product/[productId]/page.tsx
@@ -3,7 +3,8 @@ import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useEffect, useState } from "react";
 import { getProductDetail } from "@/lib/store";
 import { client } from "@/api/baseConfig";
-import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useLocale } from "next-intl";
 
 const page = ({
   params,
@@ -12,6 +13,8 @@ const page = ({
     productId: string;
   };
 }) => {
+  const router = useRouter();
+  const locale = useLocale();
   const [addToCartValue, setAddToCartValue] = useState("Add to cart");
   const dispatch = useAppDispatch();
   const product = useAppSelector((state) => state.product.product);
@@ -27,31 +30,52 @@ const page = ({
     setAddToCartValue("Added to Cart");
     console.log(response.data);
   };
+  const buyNow = async (productId: number) => {
+    const response = await client.post(
+      "http://127.0.0.1:8000/cart/add_to_cart/",
+      {
+        product_id: productId,
+        quantity: 1,
+      }
+    );
+    router.push(`/${locale}/cart`);
+    console.log(response.data);
+  };
   useEffect(() => {
     dispatch(getProductDetail(params.productId));
   }, [dispatch, params.productId]);
   return (
-    <div>
+    <div className="mt-[100px]">
       <div className="flex flex-col justify-center lg:flex-row gap-16 px-3.5">
-        <img src={product.image} alt="product image" className="w-64 h-64" />
+        <img
+          src={product.image}
+          alt="product image"
+          className="w-[450px] h-[350px] rounded-lg"
+        />
         <div className="flex justify-center">
           <div className="flex flex-col gap-5 max-w-[420px]">
             <h1 className="text-2xl font-semibold text-black">
               {product.name}
             </h1>
-            <h1 className="text-2xl text-black">{product.price}</h1>
-            <p className="text-sm text-black">{product.description}</p>
+            <h1 className="text-md text-red-900">${product.price}</h1>
+            <p className="text-md text-black">{product.description}</p>
             {isAuth && (
               <>
                 <hr className="border-black" />
                 <button
-                  className="bg-orange-500 text-white"
+                  className="bg-orange-500 text-white rounded-lg py-2 px-4"
                   type="button"
                   onClick={() => addToCart(product.id)}
                 >
                   {addToCartValue}
                 </button>
-                <button className="bg-blue-950 text-white">Buy Now</button>
+                <button
+                  className="bg-blue-950 text-white text-md rounded-lg py-2 px-4"
+                  type="button"
+                  onClick={() => buyNow(product.id)}
+                >
+                  Buy Now
+                </button>
               </>
             )}
           </div>
--- ecom/frontend/ecommerce/src/app/[locale]/profile/page.tsx
+++ ecom/frontend/ecommerce/src/app/[locale]/profile/page.tsx
@@ -2,13 +2,14 @@
 import { useAppDispatch } from "@/lib/hooks";
 import { userProfile, RootState } from "@/lib/store";
 import { useFormik } from "formik";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 
 const page = () => {
   const dispatch = useAppDispatch();
   const profile = useSelector((state: RootState) => state.user.user);
+  const [onSave, setOnSave] = useState<boolean>(false);
   useEffect(() => {
     const data = dispatch(userProfile());
     console.log(data);
@@ -26,6 +27,10 @@ const page = () => {
           `http://127.0.0.1:8000/users/${profile.user_id}/`,
           values
         );
+        setOnSave(true);
+        setTimeout(() => {
+          setOnSave(false);
+        }, 3000);
         console.log(response);
       } catch (error) {
         console.log(error);
@@ -33,36 +38,79 @@ const page = () => {
     },
   });
   return (
-    <div className="h-[calc(100vh-265px)] w-[90%] md:w-[700px] text-center m-auto mt-[25px]">
-      <form onSubmit={formik.handleSubmit}>
-        <h1>
-          <span>UserName: </span>
-          {profile.username}
+    <>
+      <div className="text-center mt-[50px]">
+        <h1 className="text-3xl">
+          Welcome
+          <span className="text-red-900 font-bold ml-1">
+            {profile.first_name}!
+          </span>
         </h1>
-        <input
-          type="text"
-          name="first_name"
-          placeholder={profile.first_name}
-          value={formik.values.first_name}
-          onChange={formik.handleChange}
-        />
-        <input
-          type="text"
-          name="last_name"
-          placeholder={profile.last_name}
-          value={formik.values.last_name}
-          onChange={formik.handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder={profile.email}
-          value={formik.values.email}
-          onChange={formik.handleChange}
-        />
-        <button type="submit">Save Changes</button>
-      </form>
-    </div>
+      </div>
+      <div className="h-[calc(100vh-265px)] w-[90%] md:w-[400px] m-auto mt-[25px]">
+        <div className="w-full">
+          <h1 className="text-2xl text-red-700">Edit Your Profile</h1>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="mt-3">
+              <div className="px-2 font-semibold">Username</div>
+              <input
+                type="text"
+                placeholder={profile.username}
+                value={profile.username}
+                className="w-full py-1 px-2 border-2 border-black rounded-[20px]"
+                disabled
+              />
+            </div>
+            <div className="mt-6">
+              <div className="px-2 font-semibold">First Name</div>
+              <input
+                type="text"
+                name="first_name"
+                placeholder={profile.first_name}
+                value={formik.values.first_name}
+                onChange={formik.handleChange}
+                className="w-full py-1 px-2 border-2 border-black rounded-[20px]"
+              />
+            </div>
+            <div className="mt-6">
+              <div className="px-2 font-semibold">Last Name</div>
+              <input
+                type="text"
+                name="last_name"
+                placeholder={profile.last_name}
+                value={formik.values.last_name}
+                onChange={formik.handleChange}
+                className="w-full py-1 px-2 border-2 border-black rounded-[20px]"
+              />
+            </div>
+            <div className="mt-6">
+              <div className="px-2 font-semibold">Email</div>
+              <input
+                type="email"
+                name="email"
+                placeholder={profile.email}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                className="w-full py-1 px-2 border-2 border-black rounded-[20px]"
+              />
+            </div>
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="bg-black py-2 w-full text-white rounded-[20px]"
+              >
+                Save Changes
+              </button>
+              {onSave && (
+                <div className="text-green-900 text-center mt-6">
+                  Changes made successfully.
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
