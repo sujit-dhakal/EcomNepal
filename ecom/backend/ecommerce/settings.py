36c4763807a7f277b92b803d856ee41d000985be
Sujit Ram Dhakal <sujit.dhakal.wrk@gmail.com>
2024-12-03 16:54:00 +0545

    feat: order

--- ecom/backend/ecommerce/settings.py
+++ ecom/backend/ecommerce/settings.py
@@ -159,8 +159,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "ROTATE_REFRESH_TOKENS": True,
